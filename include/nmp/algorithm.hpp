a3c1f286abfabaa043f47ea61ab052c54901be2b
gnzlbg <gonzalobg88@gmail.com>
2014-09-19 23:23:11 +0200

    non-blocking barrier test

--- include/nmp/algorithm.hpp
+++ include/nmp/algorithm.hpp
@@ -6,16 +6,16 @@
 // License, Version 1.0. (See accompanying file LICENSE_1_0.txt or copy at
 // http://www.boost.org/LICENSE_1_0.txt)
 ////////////////////////////////////////////////////////////////////////////////
-#include <nmp/algorithms/all_gather.hpp>
-#include <nmp/algorithms/all_reduce.hpp>
-#include <nmp/algorithms/all_to_all.hpp>
-#include <nmp/algorithms/barrier.hpp>
-#include <nmp/algorithms/broadcast.hpp>
-#include <nmp/algorithms/exclusive_scan.hpp>
-#include <nmp/algorithms/gather.hpp>
-#include <nmp/algorithms/inclusive_scan.hpp>
-#include <nmp/algorithms/recv.hpp>
-#include <nmp/algorithms/reduce.hpp>
-#include <nmp/algorithms/scatter.hpp>
-#include <nmp/algorithms/send.hpp>
+#include <nmp/algorithm/all_gather.hpp>
+#include <nmp/algorithm/all_reduce.hpp>
+#include <nmp/algorithm/all_to_all.hpp>
+#include <nmp/algorithm/barrier.hpp>
+#include <nmp/algorithm/broadcast.hpp>
+#include <nmp/algorithm/exclusive_scan.hpp>
+#include <nmp/algorithm/gather.hpp>
+#include <nmp/algorithm/inclusive_scan.hpp>
+#include <nmp/algorithm/recv.hpp>
+#include <nmp/algorithm/reduce.hpp>
+#include <nmp/algorithm/scatter.hpp>
+#include <nmp/algorithm/send.hpp>
 ////////////////////////////////////////////////////////////////////////////////
--- include/nmp/nmp.hpp
+++ include/nmp/nmp.hpp
@@ -6,7 +6,7 @@
 // License, Version 1.0. (See accompanying file LICENSE_1_0.txt or copy at
 // http://www.boost.org/LICENSE_1_0.txt)
 ////////////////////////////////////////////////////////////////////////////////
-#include <nmp/algorithms.hpp>
+#include <nmp/algorithm.hpp>
 #include <nmp/comm.hpp>
 #include <nmp/env.hpp>
 #include <nmp/group.hpp>
